48d97fb1139ac1de4fc10be147268e0a4a3d6363
zlhunzzing <zlhunzzing@gmail.com>
2020-04-18 13:44:44 +0900

    기본설정
    - 필요없는 파일삭제
    - ESlint 적용

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "airbnb-base",
+    // 'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  rules: {
+    quotes: ["error", "single"],
+      "no-unused-vars": 1,
+    //   "import/extensions": [
+    //     "error",
+    //     "ignorePackages",
+    //     { js: "never", jsx: "never", ts: "never", tsx: "never", json: "never" },
+    //   ],
+    // },
+    // settings: {
+    //   "import/resolver": { node: { extensions: [".js", ".jsx", ".ts", ".tsx"] } },
+  },
+};
